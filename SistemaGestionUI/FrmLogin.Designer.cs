746459beadf4261878292415b705ae25fc35667c
manuelquintana2 <manu.quintanamino@gmail.com>
2023-10-23 20:12:14 -0300

    cambios

---
+++ SistemaGestionUI/FrmLogin.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SistemaGestionUI
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtUsuario = new TextBox();
+            label2 = new Label();
+            txtContraseña = new TextBox();
+            btnEnviar = new Button();
+            btnMosrar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(297, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese su Email";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(230, 103);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(298, 27);
+            txtUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(310, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña";
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(230, 209);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(298, 27);
+            txtContraseña.TabIndex = 3;
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEnviar.Location = new Point(322, 295);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(94, 40);
+            btnEnviar.TabIndex = 4;
+            btnEnviar.Text = "Enviar";
+            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // btnMosrar
+            // 
+            btnMosrar.Location = new Point(574, 209);
+            btnMosrar.Name = "btnMosrar";
+            btnMosrar.Size = new Size(94, 29);
+            btnMosrar.TabIndex = 5;
+            btnMosrar.Text = "Mostrar";
+            btnMosrar.UseVisualStyleBackColor = true;
+            btnMosrar.Click += btnMosrar_Click;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnMosrar);
+            Controls.Add(btnEnviar);
+            Controls.Add(txtContraseña);
+            Controls.Add(label2);
+            Controls.Add(txtUsuario);
+            Controls.Add(label1);
+            Name = "FrmLogin";
+            Text = "FrmLogin";
+            Load += FrmLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtUsuario;
+        private Label label2;
+        private TextBox txtContraseña;
+        private Button btnEnviar;
+        private Button btnMosrar;
+    }
+}
---
+++ SistemaGestionUI/FrmLogin.cs
@@ -0,0 +1,79 @@
+﻿using SistemaGestionBussiness;
+using SistemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestionUI
+{
+    public partial class FrmLogin : Form
+    {
+        private List<Usuario> listaUsuarios;
+        private Usuario ingresar;
+        private int cantIntentos;
+        public FrmLogin()
+        {
+            InitializeComponent();
+            this.listaUsuarios = new List<Usuario>();
+            this.cantIntentos = 3;
+            this.ingresar = new Usuario();
+            this.txtContraseña.UseSystemPasswordChar = true;
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            this.listaUsuarios = UsuariosBussiness.ListarUsuarios();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            this.ingresar.Mail = txtUsuario.Text;
+            this.ingresar.Contraseña = txtContraseña.Text;
+            bool retorno = false;
+
+            foreach (Usuario usuario in listaUsuarios)
+            {
+                if (this.ingresar.Mail == usuario.Mail && this.ingresar.Contraseña == usuario.Contraseña)
+                {
+                    this.ingresar = usuario;
+                    MessageBox.Show($"Bienvenido {this.ingresar.Nombre}", "Ingreso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FrmProducto frm = new FrmProducto();
+                    retorno = true;
+                    this.Hide();
+                    frm.ShowDialog();
+                    this.DialogResult = DialogResult.OK;
+                    break;
+                }
+            }
+            if (retorno == false && this.cantIntentos > 1)
+            {
+                cantIntentos--;
+                MessageBox.Show($"Contraseña incorrecta, quedan {this.cantIntentos} intentos", "Incorrecto");
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void btnMosrar_Click(object sender, EventArgs e)
+        {
+            if (this.txtContraseña.UseSystemPasswordChar)
+            {
+                this.txtContraseña.UseSystemPasswordChar = !this.txtContraseña.UseSystemPasswordChar;
+                this.btnMosrar.Text = "Ocultar";
+            }
+            else
+            {
+                this.txtContraseña.UseSystemPasswordChar = !this.txtContraseña.UseSystemPasswordChar;
+                this.btnMosrar.Text = "Mostrar";
+            }
+        }
+    }
+}
